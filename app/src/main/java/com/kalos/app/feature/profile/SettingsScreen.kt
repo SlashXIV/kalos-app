12047781a10c01a2ed9f604d824d61afd2ddb9a9
SlashXIV <104368113+SlashXIV@users.noreply.github.com>
2026-05-22 17:24:11 +0200

    chore: bump displayed version to 1.5.1

--- app/src/main/java/com/kalos/app/feature/profile/SettingsScreen.kt
+++ app/src/main/java/com/kalos/app/feature/profile/SettingsScreen.kt
@@ -166,7 +166,7 @@ fun SettingsScreen(
             SettingsItem(
                 icon = Icons.Filled.Info,
                 title = "Version",
-                subtitle = "Kalos 1.6.0",
+                subtitle = "Kalos 1.5.1",
                 enabled = false,
             )
         }
